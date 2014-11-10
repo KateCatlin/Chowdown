00e4fc943161517dfad87ec802e3f5505b97bcac
Cory Gorham <cory.gorham@detroitlabs.com>
2014-11-10 15:14:06 -0500

    adding up button to settings activity

--- app/src/main/java/com/example/chowdown/activities/SettingsActivity.java
+++ app/src/main/java/com/example/chowdown/activities/SettingsActivity.java
@@ -2,6 +2,8 @@ package com.example.chowdown.activities;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.v4.app.NavUtils;
+import android.view.MenuItem;
 
 import com.example.chowdown.fragments.SettingsFragment;
 
@@ -12,10 +14,25 @@ public class SettingsActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getActionBar().setDisplayHomeAsUpEnabled(true);
 
         // Display the fragment as the main content.
         getFragmentManager().beginTransaction()
                 .replace(android.R.id.content, new SettingsFragment())
                 .commit();
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        int id = item.getItemId();
+        if (id == android.R.id.home){
+            NavUtils.navigateUpFromSameTask(this);
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+
+
     }
 }
