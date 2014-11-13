5b30dcb728681b4a7874432a6de775827fc25c18
Kate Catlin <kate.catlin@detroitlabs.com>
2014-11-13 16:18:04 -0500

    Adding pizza background

--- app/src/main/java/com/example/chowdown/activities/LunchDetailActivity.java
+++ app/src/main/java/com/example/chowdown/activities/LunchDetailActivity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.app.Fragment;
 import android.app.FragmentManager;
 import android.content.Intent;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.os.CountDownTimer;
 import android.support.v4.app.NavUtils;
@@ -36,6 +37,11 @@ public class LunchDetailActivity extends Activity{
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        Drawable pizza = getResources().getDrawable(R.drawable.pizza);
+        pizza.setAlpha(30);
+
+// setting the images on the ImageViews
 
         getActionBar().setDisplayHomeAsUpEnabled(true);
 
